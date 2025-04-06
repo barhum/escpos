a626b0ffffe136a1fe1b16d8209c837a77e90441
Moneer <mbcomm@gmail.com>
2025-04-05 22:04:50 -0600

    Update helpers.rb

--- lib/escpos/helpers.rb
+++ lib/escpos/helpers.rb
@@ -5,12 +5,16 @@ module Escpos
     # Encodes UTF-8 string to encoding acceptable for the printer
     # The printer must be set to that encoding
     # Available encodings can be listed in console using Encoding.constants
-    def encode(data, opts = {})
-      data.encode(opts.fetch(:encoding), 'UTF-8', {
-        invalid: opts.fetch(:invalid, :replace),
-        undef: opts.fetch(:undef, :replace),
-        replace: opts.fetch(:replace, '?')        
-      })
+    def encode(data, **opts)
+      data.encode(
+        opts.fetch(:encoding, Encoding::UTF_8),  # Added default encoding
+        'UTF-8', 
+        {
+          invalid: opts.fetch(:invalid, :replace),
+          undef: opts.fetch(:undef, :replace),
+          replace: opts.fetch(:replace, '?')        
+        }
+      )
     end
 
     # Set printer encoding
